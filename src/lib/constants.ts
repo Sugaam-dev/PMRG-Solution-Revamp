005cfeff104459ca44fe0c65107ee072de2d4b26
ghumareomkar <ghumareomkar@users.noreply.github.com>
2026-07-29 17:01:54 +0530

    Enhance logo styling, update social links, and add interactive hover animation

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -21,8 +21,8 @@ export const COMPANY = {
   ],
   social: {
     linkedin: "https://www.linkedin.com/company/pmrg-solution/",
-    facebook: "https://www.facebook.com/people/Sugaam-In/pfbid02MDjVo1ZDWVKJBJSR4cvnb5FYV94bXowvYji8nQeKkgez8DnuTQ9w7RE4QoSCjKNcl/",
-    instagram: "https://www.instagram.com/pmrgsolution?igsh=MWhzbDRxaWx1MXdqdw==",
+    facebook: "https://www.facebook.com/share/1Qy3Nfqa7e/?mibextid=wwXIfr",
+    instagram: "https://www.instagram.com/pmrg.solution/",
     whatsapp: "https://wa.me/917722017100",
   },
   url: "https://www.pmrgsolution.com",
